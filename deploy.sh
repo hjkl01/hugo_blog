cd779fc526e5c4488ed1fc6dcbe041458960cb76
ycm76229 <ycm76229@gmail.com>
2022-06-06 10:11:24 +0800

    update shell: push to branch gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+if [[ -e ./themes/learn ]]; then
+    pass
+else
+    git clone https://github.com/matcornic/hugo-theme-learn.git themes/learn
+fi
 
-# Build the project.
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    # exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
 hugo -D
 
-# Add changes to git.
-git add -A
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:hjkl01/hjkl01.github.io.git gh-pages
+echo "Push to origin"
+git push origin gh-pages
